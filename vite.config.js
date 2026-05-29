c2dbd16146da6e349d9b6740de379c19f3047f2e
donkvsbin <2549496210@qq.com>
2026-05-29 13:04:04 +0800

    11

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,38 +9,6 @@ export default defineConfig({
     vue(),
     // vueDevTools 已禁用，如需调试可取消注释下一行
     // import('vite-plugin-vue-devtools').then(m => m.default()),
-    viteImagemin({
-      // 图片压缩配置
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-      // WebP 转换配置
-      webp: {
-        quality: 80,
-      },
-    }),
   ],
   resolve: {
     alias: {
